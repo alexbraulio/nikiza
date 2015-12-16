294a9464ad8220cf2d2833880a1997f602e3664b
nadaparafazer <worldvision3d@gmail.com>
2015-12-16 10:10:32 -0200

    Signed-off-by: nadaparafazer <worldvision3d@gmail.com>

--- enviaCadastraEmail.php
+++ enviaCadastraEmail.php
@@ -1,21 +1,16 @@
 <?php
 
 // Passando os dados obtidos pelo formulário para as variáveis abaixo
-$nomeremetente     = $_POST['nomeremetente'];
 $emailremetente    = trim($_POST['emailremetente']);
 $emaildestinatario = 'contato@nikiza.xyz'; // Digite seu e-mail aqui, lembrando que o e-mail deve estar em seu servidor web
-$telefone      	   = $_POST['telefone'];
-$assunto          = $_POST['assunto'];
-$mensagem          = $_POST['mensagem'];
+
  
  
 /* Montando a mensagem a ser enviada no corpo do e-mail. */
 $mensagemHTML = '<P>FORMULARIO PREENCHIDO NO SITE WWW.NIKIZA.XYZ</P>
-<p><b>Nome:</b> '.$nomeremetente.'
-<p><b>E-Mail:</b> '.$emailremetente.'
-<p><b>Telefone:</b> '.$telefone.'
-<p><b>Assunto:</b> '.$assunto.'
-<p><b>Mensagem:</b> '.$mensagem.'</p>
+<P>CADASTRA PARA E-MAIL PUBLICITÁRIO.. RECEBER PROMOÇÕES E INFORMAÇÕES</P>
+
+<p><b>E-Mail:</b> '.$emailremetente.'</p>
 <hr>';
 
 
@@ -25,7 +20,7 @@ $headers = "MIME-Version: 1.1\r\n";
 $headers .= "Content-type: text/html; charset=utf-8\r\n";
 $headers .= "From: $emailremetente\r\n"; // remetente
 $headers .= "Return-Path: $emaildestinatario \r\n"; // return-path
-$envio = mail($emaildestinatario, $assunto, $mensagemHTML, $headers); 
+$envio = mail($emaildestinatario, $mensagemHTML, $headers); 
  
  if($envio)
 echo "<script>location.href='index.html'</script>"; // Página que será redirecionada
